27a21268ac650efb693e332c45f2302579e2215c
roy <croyvroy@gmail.com>
2023-01-12 23:56:26 +0800

    update 27 & 66 & 136 leetcode code

--- src/array/0026. Remove Duplicates from Sorted Array.js
+++ src/array/0026. Remove Duplicates from Sorted Array.js
@@ -3,12 +3,19 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let index = 1;
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] != nums[i - 1]) {
-            nums[index] = nums[i];
+    // let index = 1;
+    // for (let i = 1; i < nums.length; i++) {
+    //     if (nums[i] != nums[i - 1]) {
+    //         nums[index] = nums[i];
+    //         index++;
+    //     }
+    // }
+    let index = nums.length ? 1 : 0;
+    nums.forEach(num => {
+        if (num > nums[index - 1]) {
+            nums[index] = num;
             index++;
         }
-    }
+    });
     return index;
 };
